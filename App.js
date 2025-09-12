6a43fe53d0a2c5c7ffe9687ca56aa66a2e1bbdfd
tedcodesm <kaahenjoroge@gmail.com>
2025-09-12 23:44:53 +0300

    agri

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import "./global.css"
+import HomePage from './screens/HomePage';
+
+
+export default function App() {
+  return (
+     <HomePage/> 
+  );
+}
+
+
